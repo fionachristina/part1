c516421a32353f8d37123ec4a7ce631a5fe84fb5
fiona <38242986+fionachristina@users.noreply.github.com>
2022-08-08 23:55:39 +0300

    Passing state to child components
    Passing state to child components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Header = (course) =>{
   return (
     <>
@@ -25,7 +27,32 @@ const Total = (total) =>{
   )
 }
 
-const App = () => {
+const Hello = (props) =>{
+  const bornYear = () =>{
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+  return(
+    <>
+      <p>Hello {props.name}, you are {props.age} years</p>
+      <p>So you're probably born in {bornYear()}</p>
+    </>
+  )
+}
+
+const Hi = ({name, age}) =>{
+  const bornYear = new Date().getFullYear() - age
+  return(
+    <>
+    <p>
+      Hi, {name}, you are {age} years old. So that means you were born in {bornYear}
+    </p>
+    </>
+  )
+
+}
+
+const App = (props) => {
 
   // const courses = 'Half Stack application development'
   // const parts1 = 'Fundamentals of React'
@@ -35,7 +62,17 @@ const App = () => {
   // const parts3 = 'State of a component'
    const exercises3 = 14
 
+  const [counter, setCounter] = useState(0)
 
+  const increase = () => setCounter(counter + 1)
+
+  const reset = () => setCounter(0)
+
+  // setTimeout(() => {
+  //   setCounter(counter + 1)
+  // },1000);
+
+  // console.log('rendering...', counter)
 
   const course = 'Half Stack application development'
   const part1 = {
@@ -55,12 +92,30 @@ const App = () => {
 
   const parts = part.map((value) => [value.name, value.exercises]  )
 
-  console.log(parts)
+  // console.log(parts)
+// console.log(parts)
+  const name = 'Fiona'
+  const age = 23
+
+  const handleClick = () =>{
+    console.log('clicked')
+  }
 
   return (
     <div>
-     
-  
+
+      <button onClick={increase}>
+        plus {counter}
+      </button >
+      <button onClick={reset}> Zero</button>
+      <button onClick={() =>
+    console.log('clicked')}>Console</button>
+
+    
+      {/* <div>{counter}</div> */}
+      <Hi name = {name} age = {age}/>
+      <Hello name = {name} age = {age}/>
+      <Hello name="Maya" age={26 + 10} />
       <Header course={course}/>
       <Content parts />
       <Content name={parts} />
