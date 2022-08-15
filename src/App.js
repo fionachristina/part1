3f48f97a845b73453aa89464ac38245518c91bcb
fiona <38242986+fionachristina@users.noreply.github.com>
2022-08-15 17:38:40 +0300

    update app.js - first part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import { useState } from 'react'
 import Notes from './components/Note'
 
-const Course = ({course}) => {
-console.log({course})
+const Header = (props) =>{
   return(
     <div>
-      <h1>{course.name}</h1>
-      <div>{course.name}</div>
+      <h1>{props.course.name}</h1>
     </div>
   )
 
 }
+
+const Part =(props) =>{
+  return(
+    <div>{props.name} {props.exercises}</div>
+  )
+}
+
+const Content = (props) =>{
+  return(
+    <div>
+    {props.course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>)}
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  const parts = props.course.parts.map(course => course.exercises)
+  return (
+    
+    <div>
+<p>Total of {parts.reduce((s, p) => s + p)} exercises</p>
+    </div>
+    
+  )
+}
+
+const Course = (props) =>{
+  return(
+    <div>
+      <Header course={props.course}/>
+      <Content  course={props.course}/>
+      <Total  course={props.course}/>
+    </div>
+  )
+
+}
+
 
 const App = (props ) => {
 
@@ -57,13 +92,26 @@ const App = (props ) => {
       name: 'State of a component',
       exercises: 14,
       id: 3
+    },
+    {
+      name: 'Redux',
+      exercises: 11,
+      id: 4
     }
   ]
 }
 
+const total = course.parts.reduce((sum, part) => {
+  sum = sum + part.exercises
+  console.log('Sum', sum, part)
+  return sum 
+},0)
+
   return (
     <div>
+      
       <Course course={course} />
+
       <h1>Notes</h1>
 
       {/* <ul>
